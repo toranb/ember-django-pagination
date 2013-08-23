f5d2b8bf3624fb7722661da1798affeca1ebcc74
toran billups <toranb@gmail.com>
2013-08-23 18:34:27 -0500

    removed wsgi.py

--- ember/settings.py
+++ ember/settings.py
@@ -59,9 +59,6 @@ MIDDLEWARE_CLASSES = (
 
 ROOT_URLCONF = 'ember.urls'
 
-# Python dotted path to the WSGI application used by Django's runserver.
-WSGI_APPLICATION = 'ember.wsgi.application'
-
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
